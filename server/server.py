d79e7b97691b466e33e78477e81e301ff4741c1c
e5l <leonid.stashevsky@gmail.com>
2016-10-10 20:26:43 +0300

    python + es integration, web frontend

--- server/server.py
+++ server/server.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from Search import Search
+
+
+empty = []
+agent = Search()
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    query = request.args.get('query')
+    search_result = empty
+    if query is not None and len(query) > 0:
+        search_result = agent.request(query)
+        print("Search for: {}".format(query))
+
+    return render_template("index.html", search_results=search_result)
 
 if __name__ == "__main__":
     app.run()
